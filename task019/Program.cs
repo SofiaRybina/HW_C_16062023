b4fe32e8f8f9b320b0b47487536f6e5109a3ab1a
SofiaRybina <135506751+SofiaRybina@users.noreply.github.com>
2023-06-30 18:46:14 +0300

    Update Program.cs
    Альтернативная проверка на палиндром

--- task019/Program.cs
+++ task019/Program.cs
@@ -49,3 +49,46 @@ void Palindrom(int num)
     }
 }
 
+////Альтернатива, математическая:
+/*
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    string? value = Console.ReadLine();
+    int number = Convert.ToInt32(value);
+    return number;
+}
+
+int ReverseNumber(int a)
+{
+    int revNum = 0;
+    int temp = a;
+    while(temp > 0)
+    {
+        revNum = revNum * 10 + temp % 10; // 0+1 // 10+2=12 // 120+3=123 // 1230+2 // 12320+1 //12321
+        temp /= 10; // temp = temp/10 // 1232 // 123 // 12 // 1 // 0
+    }
+    return revNum;
+}
+
+int a = Prompt("Введите 5-значное число: ");
+int reverse = ReverseNumber(a);
+
+if (a > 9999 && a< 1000000)
+{
+    if(reverse == a)
+    {
+        Console.WriteLine("Число является палиндромом");    
+    }
+    else
+    {
+        Console.WriteLine("Число НЕ является палиндромом"); 
+    }
+
+}
+else
+{
+    Console.WriteLine("Вы ввели не 5-значное число!");   
+}
+*/
+
